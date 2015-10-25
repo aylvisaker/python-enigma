338050b6855007060a103704612fb28bd117a878
Professor Ylvisaker <ylvisaker@TARDIS.netgear.com>
2015-10-24 23:41:32 -0500

    separate interface module

--- enigma.py
+++ enigma.py
@@ -1,5 +1,4 @@
 import time
-import tkinter as tk
 
 letters = 'abcdefghijklmnopqrstuvwxyz'
 let = {i:letters[i] for i in range(26)}
@@ -15,6 +14,8 @@ rV = 'VZBRGITYUPSDNHLXAWMJQOFECK'.lower()
 rVI = 'JPGVOUMFYQBENHZRDKASXLICTW'.lower()
 rVII = 'NZJHGRCXMYSWBOUFAIVLPEKQDT'.lower()
 rVIII = 'FKQHTLXOCBJSPDZRAMEWNIUYGV'.lower()
+
+notches = [['q'],['e'],['v'],['j'],['z'],['z','m'],['z','m'],['z','m']]
 
 rA = 'EJMZALYXVBWFCRQUONTSPIKHGD'.lower()
 rB = 'YRUHQSLDPXNGOKMIEBFZCWVJAT'.lower()
@@ -288,56 +289,3 @@ print('rings    / contradiction2: ',stops,len(stops),time.clock() - t)
 plain = ''
 cipher = ''
 initial = [num[x] for x in ['y','y','e']]
-
-# FUNCTIONING ENIGMA MACHINE
-def onKeyPress(event):
-    global plain, cipher, current
-    x = event.char
-    if x == '0':
-        plain = plain[:-1]
-    if x == '1':
-        plain = ''
-    if x == '7': initial[0] = (initial[0] + 1)%26
-    if x == '8': initial[1] = (initial[1] + 1)%26
-    if x == '9': initial[2] = (initial[2] + 1)%26
-    if (x not in letters): x = ''
-    plain = plain + x
-    current = [x for x in initial]
-    for p in plain:
-        current = rotorstep(current)
-    cipher = enigmaC([let[x] for x in initial], plain)
-    a = current[0] - rings[0]
-    b = current[1] - rings[1]
-    c = current[2] - rings[2]
-    text.delete(1.0,'end')
-    text.insert('end',   'initial positions:    ' + ''.join([let[x] for x in initial]))
-    text.insert('end', '\nring settings:        ' + ''.join(rin) + '\n')
-    text.insert('end', '\n' + plain)
-    text.insert('end', '\n' + cipher + '\n')
-    text.insert('end', '\ncurrent position:     ' + ''.join([let[x] for x in current]))
-    text.insert('end', '\nplaintext alphabet:   ' + letters)
-    text.insert('end', '\nplugboard:            ' + ''.join([let[pb[x]] for x in range(26)]))
-    text.insert('end', '\nright rotor:          ' + ''.join([let[(r2[(x + c)%26] - c)%26] for x in range(26)]))
-    text.insert('end', '\nmiddle rotor:         ' + ''.join([let[(r1[(x + b)%26] - b)%26] for x in range(26)]))
-    text.insert('end', '\nleft rotor:           ' + ''.join([let[(r0[(x + a)%26] - a)%26] for x in range(26)]))
-    text.insert('end', '\nreflector:            ' + ''.join([let[r[x]] for x in range(26)]))
-    text.insert('end', '\nleft rotor inverse:   ' + ''.join([let[(r0i[(x + a)%26] - a)%26] for x in range(26)]))
-    text.insert('end', '\nmiddle rotor inverse: ' + ''.join([let[(r1i[(x + b)%26] - b)%26] for x in range(26)]))
-    text.insert('end', '\nright rotor inverse:  ' + ''.join([let[(r2i[(x + c)%26] - c)%26] for x in range(26)]))
-    text.insert('end', '\nplugboard:            ' + ''.join([let[pb[x]] for x in range(26)]))
-
-
-root = tk.Tk()
-text = tk.Text(root, background='white', foreground='black', font=('Courier New', 24))
-text.pack()
-root.bind('<KeyPress>', onKeyPress)
-root.mainloop()
-
-notches = [['q'],['e'],['v'],['j'],['z'],['z','m'],['z','m'],['z','m']]
-
-# I              Q      If rotor steps from Q to R, the next rotor is advanced
-# II             E      If rotor steps from E to F, the next rotor is advanced
-# III            V      If rotor steps from V to W, the next rotor is advanced
-# IV             J      If rotor steps from J to K, the next rotor is advanced
-# V              Z      If rotor steps from Z to A, the next rotor is advanced
-# VI, VII, VIII  Z + M	If rotor steps from Z to A, or from M to N the next rotor is advanced
