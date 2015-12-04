c20286369c7f6677470af7447a6242790eb6ed10
Andrew Ylvisaker <ajylvisaker@gmail.com>
2015-12-04 09:47:45 -0600

    working on double step

--- newfile.py
+++ newfile.py
@@ -16,11 +16,11 @@ rotor = {}
 rotor['IC']     = 'DMTWSILRUYQNKFEJCAZBPGXOHV'
 rotor['IIC']    = 'HQZGPJTMOBLNCIFDYAWVEUSRKX'
 rotor['IIIC']   = 'UQNTLSZFMREHDPXKIBVYGJCWOA'
-rotor['I']      = 'JGDQOXUSCAMIFRVTPNEWKBLZYH'
-rotor['II']     = 'NTZPSFBOKMWRCJDIVLAEYUXHGQ'
-rotor['III']    = 'JVIUBHTCDYAKEQZPOSGXNRMWFL'
-rotor['UKW']    = 'QYHOGNECVPUZTFDJAXWMKISRBL'
-rotor['ETW']    = 'QWERTZUIOASDFGHJKPYXCVBNML'
+rotor['I-r']    = 'JGDQOXUSCAMIFRVTPNEWKBLZYH'
+rotor['II-r']   = 'NTZPSFBOKMWRCJDIVLAEYUXHGQ'
+rotor['III-r']  = 'JVIUBHTCDYAKEQZPOSGXNRMWFL'
+rotor['UKW-r']  = 'QYHOGNECVPUZTFDJAXWMKISRBL'
+rotor['ETW-r']  = 'QWERTZUIOASDFGHJKPYXCVBNML'
 rotor['I-K']    = 'PEZUOHXSCVFMTBGLRINQJWAYDK'
 rotor['II-K']   = 'ZOUESYDKFWPCIQXHMVBLGNJRAT'
 rotor['III-K']  = 'EHRVXGAOBQUSIMZFLYNWKTPDJC'
@@ -73,11 +73,13 @@ def enigmaC(positions,plain):
 	pos = list(positions)
 	cha = list(plain)
 	for n in range(len(cha)):
-		pos[2] = shift[pos[2]]
 		if pos[2] in notch['III']:
 			pos[1] = shift[pos[1]]
 		if pos[1] in notch['II']:
+			pos[1] = shift[pos[1]]
 			pos[0] = shift[pos[0]]
+		pos[2] = shift[pos[2]]
+		print(pos)
 		x = rot[2,pos[2],cha[n]]
 		x = rot[1,pos[1],x]
 		x = rot[0,pos[0],x]
@@ -88,25 +90,21 @@ def enigmaC(positions,plain):
 		cha[n] = x
 	return(''.join(cha))
 
-n = 5000
-longplain = ''.join([conv[random.randint(0,25)] for x in range(n)])
-t = time.clock()
-longcipher = enigmaC('XMZ',longplain)
-longdecipher = enigmaC('XMZ',longcipher)
-print(str(int(2*n / (time.clock() - t))) + ' characters per second')
-if longplain==longdecipher:
-	print('all is well')
-else: print('there was an error')
-print(longplain)
+#n = 1000
+#longplain = ''.join([conv[random.randint(0,25)] for x in range(n)])
+#t = time.clock()
+#longcipher = enigmaC('XMZ',longplain)
+#longdecipher = enigmaC('XMZ',longcipher)
+#print(str(int(2*n / (time.clock() - t))) + ' characters per second')
+#if longplain==longdecipher:
+#	print('all is well')
+#else: print('there was an error')
 
-a = [1,2,3,4,5]
-a.append(a.pop(0))
-print(a)
+print(enigmaC('ADU','AAAAAAAAAA'))
 
-import collections
-a = collections.deque([1,2,3,4,5])
-a.rotate(-1)
-print(a)
-a.rotate(-2)
-print(a)
-print(a[1])
+#a = [1,2,3,4,5]
+#a.append(a.pop(0))
+
+#import collections
+#a = collections.deque([1,2,3,4,5])
+#a.rotate(-1)
