f9b7e8e040bc5c5dc2bfa1f34e11bdd1d2bb75d7
Nicole Kingsley <nkingsle@Nicole-Kingsleys-MacBook-Pro.local>
2015-12-06 00:44:53 -0600

    added initialization function for building dictionaries

--- newfile.py
+++ newfile.py
@@ -1,6 +1,12 @@
 import random
 import time
 #import collections
+#a = [1,2,3,4,5]
+#a.append(a.pop(0))
+
+#import collections
+#a = collections.deque([1,2,3,4,5])
+#a.rotate(-1)
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'.upper()
 conv = {}
@@ -13,6 +19,7 @@ for p in alphabet:
 	shift[p] = conv[(conv[p] + 1) % 26]
 
 rotor = {}
+notch = {}
 rotor['IC']     = 'DMTWSILRUYQNKFEJCAZBPGXOHV'
 rotor['IIC']    = 'HQZGPJTMOBLNCIFDYAWVEUSRKX'
 rotor['IIIC']   = 'UQNTLSZFMREHDPXKIBVYGJCWOA'
@@ -41,8 +48,7 @@ rotor['B']      = 'YRUHQSLDPXNGOKMIEBFZCWVJAT'
 rotor['C']      = 'FVPJIAOYEDRZXWGCTKUQSBNMHL'
 rotor['B-Thin'] = 'ENKQAUYWJICOPBLMDXZVFTHRGS'
 rotor['C-Thin'] = 'RDOBJNTKVEHMLFCWZAXGYIPSUQ'
-rotor['Dum']    = alphabet
-notch = {}
+rotor['Dum']    = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 notch['I']      = ['Q']
 notch['II']     = ['E']
 notch['III']    = ['V']
@@ -55,43 +61,43 @@ notch['VIII']   = ['Z','M']
 rgs = 'ZZDG'
 rts = ['Beta','VI','I','III']
 rfs = 'B-Thin'
+plugs = 'BQ CR DIe EJ KW MT OS PX UZ GH'
 
-rot = {}
-inv = {}
-ref = {}
-dum = {}
-#for i in range(26):
-#	rot[0,'A',conv[i]] = rotor['Beta'][i]
-#	rot[1,'A',conv[i]] = rotor['VI'][i]
-#	rot[2,'A',conv[i]] = rotor['I'][i]
-#	rot[3,'A',conv[i]] = rotor['III'][i]
-#	inv[0,'A',rotor['Beta'][i]] = conv[i]
-#	inv[1,'A',rotor['VI'][i]] = conv[i]
-#	inv[2,'A',rotor['I'][i]] = conv[i]
-#	inv[3,'A',rotor['III'][i]] = conv[i]
-#	ref[conv[i]] = rotor['B-Thin'][i]
-#t = time.clock()
-for i in alphabet:
-	ref[i] = rotor[rfs][conv[i]]
-	for p in alphabet:
-		for r in range(4):
-			ii = conv[i]
-			pp = conv[p] - conv[rgs[r]]
-			rr = [conv[x] for x in rotor[rts[r]]]
-			#rot[r,p,i] = conv[(conv[rot[r,'A',conv[(conv[i] + conv[p])%26]]] - conv[p]) % 26]
-			#inv[r,p,i] = conv[(conv[inv[r,'A',conv[(conv[i] + conv[p])%26]]] - conv[p]) % 26]
-			rot[r,p,i] = conv[(rr[(ii + pp) % 26] - pp) % 26]
-			inv[r,p,rot[r,p,i]] = i
-#print(time.clock() - t)
-plugs = 'BQ CR DI EJ KW MT OS PX UZ GH'
-pb = {}
-for letter in alphabet:
-        pb[letter] = letter
-for pair in plugs.split(' '):
-        pb[pair[0]] = pair[1]
-        pb[pair[1]] = pair[0]
+def initialize(rgs,rts,rfs,plugs):
+	rot = {}
+	inv = {}
+	ref = {}
+	pb  = {}
+	for i in alphabet:
+		pb[i] = i
+		ref[i] = rotor[rfs][conv[i]]
+		for p in alphabet:
+			for r in range(4):
+				ii = conv[i]
+				pp = conv[p] - conv[rgs[r]]
+				rr = [conv[x] for x in rotor[rts[r]]]
+				rot[r,p,i] = conv[(rr[(ii + pp) % 26] - pp) % 26]
+				inv[r,p,rot[r,p,i]] = i
+	for pair in plugs.split(' '):
+	        pb[pair[0]] = pair[1]
+	        pb[pair[1]] = pair[0]
+	return([rot,inv,ref,pb])
 
-def enigmaC(positions,plain):
+def positionlist(initial,length):
+	out = []
+	pos = list(initial)
+	for n in range(length):
+		if pos[3] in notch[rts[3]]:
+                        pos[2] = shift[pos[2]]
+                elif pos[2] in notch[rts[2]]:
+                        pos[2] = shift[pos[2]]
+                        pos[1] = shift[pos[1]]
+                pos[3] = shift[pos[3]]
+		out.append(pos)
+	return(out)
+		
+
+def enigma(positions,plain):
 	pos = list(positions)
 	cha = list(plain)
 	for n in range(len(cha)):
@@ -116,23 +122,29 @@ def enigmaC(positions,plain):
 		cha[n] = x
 	return(''.join(cha))
 
-n = 10000
-longplain = ''.join([conv[random.randint(0,25)] for x in range(n)])
+rgs = 'ZZDG'
+rts = ['Beta','VI','I','III']
+rfs = 'B-Thin'
+plugs = 'BQ CR DI EJ KW MT OS PX UZ GH'
 t = time.clock()
-longcipher = enigmaC('AXMZ',longplain)
-longdecipher = enigmaC('AXMZ',longcipher)
-print(str(int(2*n / (time.clock() - t))) + ' characters per second')
-if longplain==longdecipher:
-	print('all is well')
-else: print('there was an error')
-
+[rot,inv,ref,pb] = initialize(rgs,rts,rfs,plugs)
+t = (time.clock() - t) * 1000
+print('initialization took ' + str("%.2f" % t) + ' milliseconds')
+position = 'NAQL'
 cipher = 'HCEYZTCSOPUPPZDICQRDLWXXFACTTJMBRDVCJJMMZRPYIKHZAWGLYXWTMJPQUEFSZBOTVRLALZXWVXTSLFFFAUDQFBWRRYAPSBOWJMKLDUYUPFUQDOWVHAHCDWAUARSWTKOFVOYFPUFHVZFDGGPOOVGRMBPXXZCANKMONFHXPCKHJZBUMXJWXKAUODXZUCVCXPFT'
 plain = 'BOOTKLARXBEIJSCHNOORBETWAZWOSIBENXNOVXSECHSNULCBMXPROVIANTBISZWONULXDEZXBENOETIGEGLMESERYNOCHVIEFKLHRXSTEHEMARQUBRUNOBRUNFZWOFUHFXLAGWWIEJKCHAEFERJXNNTWWWFUNFYEINSFUNFMBSTEIGENDYGUTESIWXDVVVJRASCH'
-print(enigmaC('NAQL',cipher))
-
-#a = [1,2,3,4,5]
-#a.append(a.pop(0))
-
-#import collections
-#a = collections.deque([1,2,3,4,5])
-#a.rotate(-1)
+n = 100000
+longplain = ''.join([conv[random.randint(0,25)] for x in range(n)])
+randpos = ''.join([conv[random.randint(0,25)] for x in range(4)])
+t = time.clock()
+longcipher = enigma(randpos,longplain)
+longdecipher = enigma(randpos,longcipher)
+print('mapping at ' + str(int(2*n / (time.clock() - t))) + ' characters per second')
+if longplain==longdecipher:
+	print('symmetric encryption test: pass')
+else: print('there was an error')
+cipher = 'HCEYZTCSOPUPPZDICQRDLWXXFACTTJMBRDVCJJMMZRPYIKHZAWGLYXWTMJPQUEFSZBOTVRLALZXWVXTSLFFFAUDQFBWRRYAPSBOWJMKLDUYUPFUQDOWVHAHCDWAUARSWTKOFVOYFPUFHVZFDGGPOOVGRMBPXXZCANKMONFHXPCKHJZBUMXJWXKAUODXZUCVCXPFT'
+plain = 'BOOTKLARXBEIJSCHNOORBETWAZWOSIBENXNOVXSECHSNULCBMXPROVIANTBISZWONULXDEZXBENOETIGEGLMESERYNOCHVIEFKLHRXSTEHEMARQUBRUNOBRUNFZWOFUHFXLAGWWIEJKCHAEFERJXNNTWWWFUNFYEINSFUNFMBSTEIGENDYGUTESIWXDVVVJRASCH'
+decipher = enigma(position,cipher)
+if decipher == plain: print('historical accuracy check: pass')
+else: print(decipher)
