b47db3fb0364fe3e659f28af90bcfccf3e3379f4
aylvisaker <ajylvisaker@gmail.com>
2016-11-16 18:45:14 -0600

    added rotors

--- machine.py
+++ machine.py
@@ -1,1 +1,20 @@
-print('hello world')
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let = {i:alphabet[i] for i in xrange(26)}
+num = {alphabet[i]:i for i in xrange(26)}
+rotorCollection = ['EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+                   'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+                   'BDFHJLCPRTXVZNYEIWGAKMUSQO',
+                   'ESOVPZJAYQUIRHXLNFTGKDCMWB',
+                   'VZBRGITYUPSDNHLXAWMJQOFECK',
+                   'JPGVOUMFYQBENHZRDKASXLICTW',
+                   'NZJHGRCXMYSWBOUFAIVLPEKQDT',
+                   'FKQHTLXOCBJSPDZRAMEWNIUYGV']
+rotorCollection = [[num[y] for y in x] for x in rotorCollection]
+
+class machine(object):
+    """
+    A class representing an enigma machine with rotors and reflectors configured.
+    Set the rotor positions and ready to encrypt.
+    """
+    def __init__(self, rotors):
+        print(1+1)
